fae9f77ca18291f47c8b0feaff39bdf37776b103
wangfeiping <wangfeiping@outlook.com>
2024-07-02 11:12:46 +0800

    popup & append a button

---
+++ public/popup.js
@@ -0,0 +1,7 @@
+const button = document.createElement('button');
+button.textContent = 'Click me';
+button.addEventListener('click', () => {
+            // Perform some action when the button is clicked
+});
+document.body.appendChild(button);
+
--- src/background.ts
+++ src/background.ts
@@ -4,6 +4,39 @@ function changeTheme(color: string): void {
     document.body.style.backgroundColor = color;
 }
 
+function createDomElement(html: string) {
+  const dom = new DOMParser().parseFromString(html, 'text/html');
+  return dom.body.firstElementChild;
+}
+
+async function onInit() {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
+
+  console.log("cheshire cat...");
+
+  const current = await chrome.windows.getCurrent();
+  const allTabs = await chrome.tabs.query({});
+  allTabs.forEach((tab) => {
+    if (tab.windowId == current.id) {
+        //chrome.tabs.executeScript(tab.id, {
+        //    code: `
+        //        const button = document.createElement('button');
+        //        button.textContent = 'Click me';
+        //        button.addEventListener('click', () => {
+        //            // Perform some action when the button is clicked
+        //        });
+        //        document.body.appendChild(button);
+        //    `
+        //});
+        console.log("cheshire cat!");
+     }
+   });
+}
+
+chrome.runtime.onInstalled.addListener(onInit);
+
 chrome.action.onClicked.addListener((tab) => {
     // chrome.windows.create({
     //     url: 'popup.html',
